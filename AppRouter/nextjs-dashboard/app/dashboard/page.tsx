c5179243050fc17a4eb77500965e76736ec58c23
artiff3 <158023442+GhostManSSR@users.noreply.github.com>
2025-09-30 01:17:14 +0700

    Update page.tsx

--- AppRouter/nextjs-dashboard/app/dashboard/page.tsx
+++ AppRouter/nextjs-dashboard/app/dashboard/page.tsx
@@ -2,10 +2,8 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue } from '@/app/lib/data';
 
 export default async function Page() {
-    const revenue = await fetchRevenue();
 
     return (
         <main>
@@ -28,4 +26,5 @@ export default async function Page() {
             </div>
         </main>
     );
+
 }
